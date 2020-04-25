02448493d36ac60fdac910938b884dc1ed34232a
Feruz Iskandarov <feruzbek.iskandarov@gmail.com>
2020-04-26 00:12:57 +0900

    http-api-server first bone

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"github.com/IFtech-A/http_rest_api/internal/apiserver"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.yaml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+	yamlFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatal("Error while reading a file")
+		return
+	}
+
+	config := apiserver.NewConfig()
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatal("Error while parsing config file")
+	}
+
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatal("Error on start")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/IFtech-A/http_rest_api
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/sirupsen/logrus v1.5.0
+	github.com/stretchr/testify v1.5.1
+	gopkg.in/yaml.v2 v2.2.8
+)
